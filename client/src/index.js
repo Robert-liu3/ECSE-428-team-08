e36f419f0173b77a291f8259bc7c8dec0807c912
Jerry Xia <jerrylxia@gmail.com>
2022-10-10 16:31:57 -0400

    Create components, Material UI frontend page

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
 import ReactDOM from "react-dom";
-import { render } from "react-dom";
 
 import App from "./App";
 
--- server/index.js
+++ server/index.js
@@ -2,3 +2,5 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
+
+// connect to database
