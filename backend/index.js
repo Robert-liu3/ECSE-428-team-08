5a79e47123ff8e242123cb93f252109221ba46bf
Robert-liu3 <77682309+Robert-liu3@users.noreply.github.com>
2022-10-10 18:57:58 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,4 +3,15 @@ import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 
-// connect to database
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.usrlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://ecse428_db:passwordecse428@cluster0.k783efm.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewURLParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log('Server running on port: ${PORT}')))
+    .catch((error) => console.log("error"));
