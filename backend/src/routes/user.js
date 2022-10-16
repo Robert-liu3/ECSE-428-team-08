3acdaaf42e5edb426232ed712c89b27efb62a8fa
VadimT7 <vadim.tuchila@mail.mcgill.ca>
2022-10-15 21:57:31 -0400

    Added models and changed structure of the backend folder

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/profile', (req, res) => {
     res.send('THIS WORKS');
+    
+    // Redirect the user to the profile page
 });
 
 export default router;
