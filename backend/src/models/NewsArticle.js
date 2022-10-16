40ccab7636ae48a4f85c768cef8b42b5101bd244
DannyTu2 <78064051+DannyTu2@users.noreply.github.com>
2022-10-16 00:23:25 -0400

    Added models for news by Vadim

---
+++ backend/src/models/NewsArticle.js
@@ -0,0 +1,25 @@
+import mongoose, { mongo } from "mongoose";
+
+let ArticleSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    body: String,
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    comments: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}
+},
+{
+    timestamps: true
+}); // save the createdAt and updatedAt data for the article
+
+// Populate the author field in the database
+ArticleSchema.methods.toJSONFor = (user) => {
+    return {
+        id: this._id,
+        title: this.title,
+        description: this.description,
+        body: this.body,
+        author: this.author.toProfileJSONFor(user),
+    }
+};
+
+mongoose.model('NewsArticle', ArticleSchema);s
