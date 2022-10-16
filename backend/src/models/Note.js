9c5694d9d10757a4979f556019297a0d08f62d46
VadimT7 <vadim.tuchila@mail.mcgill.ca>
2022-10-15 22:24:40 -0400

    Added note and article bookmark schemas

--- backend/src/models/Note.js
+++ backend/src/models/Note.js
@@ -1,1 +1,12 @@
 import mongoose from "mongoose";
+
+let NoteSchema = new mongoose.Schema({
+    title: String,
+    body: String,
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+},
+{
+    timestamps : true
+});
+
+mongoose.model('Note', NoteSchema);
