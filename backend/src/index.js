8e2132fde045c0874b7a4b4379486109516f4bb8
happymario123 <52861139+happymario123@users.noreply.github.com>
2022-10-16 00:40:27 -0400

    Added routes and error handling

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,12 +5,24 @@ import mongoose from "mongoose";
 
 import postRoutes from './routes/posts.js';
 
+//const notesRoutes = require('./routes/notes-routes');
+import notesRoutes from './routes/notes-routes.js'
 const app = express();
-
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use('/posts', postRoutes);
+app.use('/notes', notesRoutes);
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500)
+    res.json({message: error.message || 'An unknown error occured.'});
+});
+
 app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://ecse428_db:passwordecse428@cluster0.k783efm.mongodb.net/?retryWrites=true&w=majority';
