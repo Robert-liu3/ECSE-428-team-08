b7ea8103a9b9ee9b36837fedfcad1d057a0ad20c
NoahYe123 <jian.ye@mail.mcgill.ca>
2022-10-16 21:34:48 -0400

    added creatUser endpoint and gitignore

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -5,17 +5,17 @@ const users = [];
 
 export const getUser = async (req, res) => {
   try {
-    res.json({users})
+    res.json({ users });
   } catch (error) {
     res.json({ message: error.message });
   }
 };
 
 export const createUser = async (req, res) => {
-  const userInfo  = req.body;
+  const userInfo = req.body;
   console.log(userInfo);
-  users.push({image: userInfo.image, profileBio: userInfo.profileBio});
-  console.log(users)
+  users.push({ image: userInfo.image, profileBio: userInfo.profileBio });
+  console.log(users);
 
   const newUser = new user(userInfo);
 
