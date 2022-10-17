5fcc002b50f6ce0fe170a8340ab25a8159db5d08
Jerry Xia <jerrylxia@gmail.com>
2022-10-17 16:08:16 -0400

    Inject Chart HTML into Welcome page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,17 @@
-import NavBar from './components/Nav/NavBar.js'
-import Welcome from './components/Welcome/Welcome.js'
-import Charts from './components/Chart/Chart.js'
-import News from './components/News/News.js'
-import './components/site_style.css'
-import { Route, Routes } from 'react-router-dom'
+import NavBar from "./components/Nav/NavBar.js";
+import Welcome from "./components/Welcome/Welcome.js";
+import News from "./components/News/News.js";
+import "./components/site_style.css";
+import { Route, Routes } from "react-router-dom";
 
 function App(props) {
   return (
     <>
-      <NavBar/>
-      <div className='container'>
+      <NavBar />
+      <div className="container">
         <Routes>
-          <Route path="/" element={<Welcome/>}/>
-          <Route path="/charts" element={<Charts/>}/>
-          <Route path="/news" element={<News/>}/>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/news" element={<News />} />
         </Routes>
       </div>
     </>
