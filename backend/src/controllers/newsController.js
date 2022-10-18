875024bd742bf8eb939ab5b76742cda51df291a5
Capt0bvious <abiola.olaniyan15@gmail.com>
2022-10-18 00:31:14 -0400

    Added comment

--- backend/src/controllers/newsController.js
+++ backend/src/controllers/newsController.js
@@ -4,6 +4,7 @@ const newsapi = new NewsAPI('4e01875ffe8c42ef878ccd83a054fd00');
 export const getNews = async (req, res) => {
     let articles = {};
     try {
+        // get articles from API
         await newsapi.v2.topHeadlines({
             q: 'stocks',
             category: 'business',
@@ -12,7 +13,6 @@ export const getNews = async (req, res) => {
             articles = response;
         });
 
-        // console.log(articles);
         res.json({ articles });
     } catch (error) {
         res.json({ message: error.message });
