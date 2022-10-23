9027fb6ed213b9adb8c7b5e90447bfac2bfb5f7b
hiArturo <arturo.moryramirez@mail.mcgill.ca>
2022-10-23 12:35:13 -0400

    Login and signup page layout fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import NavBar from './components/Nav/NavBar.js'
 import Welcome from './components/Welcome/Welcome.js'
 import Charts from './components/Chart/Chart.js'
 import News from './components/News/News.js'
+import UserLogin from './components/UserLogin/UserLogin.js'
+import UserSignUp from './components/UserSignUp/UserSignUp.js'
 import './components/site_style.css'
 import { Route, Routes } from 'react-router-dom'
 
@@ -14,6 +16,8 @@ function App(props) {
           <Route path="/" element={<Welcome/>}/>
           <Route path="/charts" element={<Charts/>}/>
           <Route path="/news" element={<News/>}/>
+          <Route path="/login" element={<UserLogin/>}/>
+          <Route path="/signup" element={<UserSignUp/>}/>
         </Routes>
       </div>
     </>
---
+++ frontend/src/components/UserLogin/UserLogin.js
@@ -0,0 +1,223 @@
+import * as React from "react";
+
+export default function MyComponent(props) {
+  return (
+    <>
+      <div className="div">
+        <div className="div-2">
+          <div className="div-3">
+            <div className="div-4">StockZ</div>
+          </div>
+          <div className="div-5">
+            <div className="div-6">Username/email</div>
+            <div className="div-7">
+              <div className="div-8" />
+            </div>
+            <div className="div-9">Password</div>
+            <div className="div-10">
+              <div className="div-11" />
+            </div>
+          </div>
+          <div className="div-12">
+            <div className="div-13">
+              <div className="div-14">Login</div>
+            </div>
+            <div className="div-15">
+              <div className="div-16">Sign Up</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        .div {
+          display: flex;
+          flex-direction: column;
+          max-width: 1920px;
+          justify-content: center;
+          align-items: center;
+          padding-top: 54px;
+          padding-right: 446px;
+          padding-bottom: 54px;
+          padding-left: 446px;
+          background-color: rgba(255, 255, 255, 1);
+        }
+        .div-2 {
+          display: flex;
+          flex-direction: column;
+          max-width: 500px;
+          justify-content: center;
+          align-items: center;
+        }
+        .div-3 {
+          display: flex;
+          flex-direction: column;
+          max-width: 240px;
+          justify-content: flex-start;
+          align-items: flex-start;
+        }
+        .div-4 {
+          display: flex;
+          flex-direction: row;
+          max-width: 220px;
+          justify-content: flex-start;
+          align-items: flex-start;
+          padding-top: 10px;
+          padding-right: 10px;
+          padding-bottom: 10px;
+          padding-left: 10px;
+          color: rgba(115, 194, 251, 1);
+          font-size: 64px;
+          letter-spacing: 0%;
+          text-align: left;
+          font-family: "Inter", sans-serif;
+        }
+        .div-5 {
+          display: flex;
+          flex-direction: column;
+          max-width: 387px;
+          justify-content: flex-start;
+          align-items: center;
+          margin-top: 16px;
+        }
+        .div-6 {
+          display: flex;
+          flex-direction: row;
+          max-width: 260px;
+          justify-content: flex-start;
+          align-items: flex-start;
+          padding-top: 10px;
+          padding-right: 10px;
+          padding-bottom: 10px;
+          padding-left: 10px;
+          color: rgba(0, 0, 0, 1);
+          font-size: 32px;
+          letter-spacing: 0%;
+          text-align: left;
+          font-family: "Raleway", sans-serif;
+        }
+        .div-7 {
+          display: flex;
+          flex-direction: column;
+          max-width: 387px;
+          justify-content: flex-start;
+          align-items: flex-start;
+          margin-top: 16px;
+          padding-top: 10px;
+          padding-right: 10px;
+          padding-bottom: 10px;
+          padding-left: 10px;
+        }
+        .div-8 {
+          display: flex;
+          max-width: 367px;
+          height: 48px;
+          width: 367px;
+          border-radius: 12px;
+          background-color: rgba(237, 239, 242, 1);
+        }
+        .div-9 {
+          display: flex;
+          flex-direction: row;
+          max-width: 147px;
+          justify-content: flex-start;
+          align-items: flex-start;
+          margin-top: 16px;
+          padding-top: 10px;
+          padding-right: 10px;
+          padding-bottom: 10px;
+          padding-left: 10px;
+          color: rgba(0, 0, 0, 1);
+          font-size: 32px;
+          letter-spacing: 0%;
+          text-align: left;
+          font-family: "Raleway", sans-serif;
+        }
+        .div-10 {
+          display: flex;
+          flex-direction: column;
+          max-width: 387px;
+          justify-content: flex-start;
+          align-items: flex-start;
+          margin-top: 16px;
+          padding-top: 10px;
+          padding-right: 10px;
+          padding-bottom: 10px;
+          padding-left: 10px;
+        }
+        .div-11 {
+          display: flex;
+          max-width: 367px;
+          height: 48px;
+          width: 367px;
+          border-radius: 12px;
+          background-color: rgba(237, 239, 242, 1);
+        }
+        .div-12 {
+          display: flex;
+          flex-direction: column;
+          max-width: 175px;
+          justify-content: center;
+          align-items: center;
+          margin-top: 16px;
+        }
+        .div-13 {
+          display: flex;
+          flex-direction: row;
+          max-width: 125px;
+          justify-content: center;
+          align-items: center;
+          padding-top: 10px;
+          padding-right: 10px;
+          padding-bottom: 10px;
+          padding-left: 10px;
+          background-color: rgba(15, 82, 186, 1);
+        }
+        .div-14 {
+          display: flex;
+          flex-direction: row;
+          max-width: 85px;
+          justify-content: center;
+          align-items: center;
+          padding-top: 10px;
+          padding-right: 10px;
+          padding-bottom: 10px;
+          padding-left: 10px;
+          color: rgba(255, 255, 255, 1);
+          font-size: 32px;
+          letter-spacing: 0%;
+          text-align: left;
+          font-family: "Raleway", sans-serif;
+        }
+        .div-15 {
+          display: flex;
+          flex-direction: row;
+          max-width: 159px;
+          justify-content: center;
+          align-items: center;
+          margin-top: 16px;
+          padding-top: 10px;
+          padding-right: 10px;
+          padding-bottom: 10px;
+          padding-left: 10px;
+          background-color: rgba(15, 82, 186, 1);
+        }
+        .div-16 {
+          display: flex;
+          flex-direction: row;
+          max-width: 150px;
+          justify-content: center;
+          align-items: center;
+          padding-top: 10px;
+          padding-right: 10px;
+          padding-bottom: 10px;
+          padding-left: 10px;
+          color: rgba(255, 255, 255, 1);
+          font-size: 32px;
+          letter-spacing: 0%;
+          text-align: left;
+          font-family: "Raleway", sans-serif;
+        }
+      `}</style>
+    </>
+  );
+}
