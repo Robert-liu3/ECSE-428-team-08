efcaf991ecadcb5fbb0c0eb72d61f8e034614fb2
Robert-liu3 <77682309+Robert-liu3@users.noreply.github.com>
2022-10-23 22:03:47 -0400

    Revert "Add chart widget"

--- frontend/src/components/Chart/Chart.js
+++ frontend/src/components/Chart/Chart.js
@@ -1,15 +1,14 @@
 import React from "react";
-import "./styles.css";
-
 function iframe() {
   return {
-    __html: '<iframe src="./Chart-Widget-for-Frontend.html" ></iframe>',
+    __html:
+      '<iframe src="./Chart-Widget-for-Frontend.html" width="1200" height="600" ></iframe>',
   };
 }
 
 export default function Charts() {
   return (
-    <div class="h_iframe">
+    <div>
       <div dangerouslySetInnerHTML={iframe()} />
     </div>
   );
