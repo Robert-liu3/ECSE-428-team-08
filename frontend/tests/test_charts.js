a92c0df3b9e9a4bac96ed3567133b286d2ff4b13
Cedric Barre <cedbarr@gmail.com>
2022-10-29 13:05:54 -0400

    Workflow changes to make selenium work with github actions

--- frontend/tests/test_charts.js
+++ frontend/tests/test_charts.js
@@ -1,4 +1,30 @@
-require('chromedriver');
+// require('chromedriver');
 const {Builder, By, Key, until} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome')
 
-let driver = new Builder().forBrowser('chrome').build();
+const screen = {
+    width: 640,
+    height: 480
+  };
+
+const run_chart_tests = async () => {
+    let driver = await new Builder().forBrowser('chrome')
+                                    .setChromeOptions(new chrome.Options().headless().windowSize(screen))
+                                    .build();
+
+    await driver.get("http:/www.google.com");
+
+    var promise = driver.getTitle();
+
+    promise.then(function(title) 
+
+    {
+
+    console.log(title);
+
+    });
+
+    await driver.quit();
+}
+
+run_chart_tests()
