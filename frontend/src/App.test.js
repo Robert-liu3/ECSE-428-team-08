9f7befa31ebadbdc2953e2b08230aa7c0438481b
Cedric Barre <cedbarr@gmail.com>
2022-10-29 15:23:03 -0400

    Set up proper testing architecture for frontend

---
+++ frontend/src/App.test.js
@@ -0,0 +1,33 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome')
+
+const screen_obj = {
+    width: 640,
+    height: 480
+  };
+
+const run_chart_tests = async () => {
+    let driver = await new Builder().forBrowser('chrome')
+                                    .setChromeOptions(new chrome.Options().headless().windowSize(screen_obj))
+                                    .build();
+
+    await driver.get("http://localhost:3000/");
+
+    var title = await driver.getTitle();
+
+    await driver.quit();
+
+    return title;
+}
+
+test('dummy', () => {
+    console.log("Hello");
+
+    expect(true).toBe(true);
+});
+
+test('charts', async() => {
+    let title = await run_chart_tests();
+    console.log(title);
+    expect(title).toBe('React App');
+});
