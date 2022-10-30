60ef4742b3e0393079869fea72aa5fc8dd098d4b
miki <michelle.lee6@mail.mcgill.ca>
2022-10-30 16:50:51 -0400

    Added Cypress tests for frontend

---
+++ frontend/cypress/e2e/spec.cy.js
@@ -0,0 +1,22 @@
+/**********************
+TESTING NEWS PAGE FRONTEND
+**********************/
+
+describe('News Page Frontend', () => {
+  it('Loading the News Page', () => {
+    cy.visit('http://localhost:3000/news')
+  })
+
+  it('Checking if page contains "Latest"', () => {
+    cy.contains('Latest')
+  })
+
+  //checking if we can click on the first link but cypress doesnt allow changing urls  
+  // it('Clicking on news', () => {
+  //   cy.get('.post-media')
+  //     .get('a[href*="https"]').first()
+  //   .click()
+  // })
+
+
+})
