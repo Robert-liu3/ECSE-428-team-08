8c5f0d0299d73d18aba38e318589e590e3ebb181
Robert-liu3 <robert.liu3@mail.mcgill.ca>
2022-10-30 12:31:22 -0400

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,39 +12,12 @@ app.use(cors()); // CORS policy
 app.use(express.json()); 
 app.use(express.urlencoded());
 app.use('/', userRouter);
-
-
-
-
-// header('Access-Control-Allow-Origin: *')
-// header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE')
-// header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization')
-// var corsOptions = {
-//     origin: 'http://localhost:3000',
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-//   }
-
-
-
-
-// app.listen(5000, ()=>{
-// console.log("sever started on 5000")});
-
-const app = express();
-app.use(cors());
-
 app.use('/posts', postRoutes);
 app.use('/news', newsRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-
-
-
 // connect to database
-
-
 const CONNECTION_URL = 'mongodb+srv://ecse428_db:passwordecse428@cluster0.k783efm.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
