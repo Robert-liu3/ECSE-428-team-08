960e545e55d46b9c307db761d0b3c6b33c6043a3
DannyTu2 <78064051+DannyTu2@users.noreply.github.com>
2022-10-30 16:59:27 -0400

    Added tests for backend for sources, category

--- backend/src/tests/News.test.js
+++ backend/src/tests/News.test.js
@@ -1,11 +1,53 @@
 import { app } from "../index.js"
 import request from "supertest"
 
+
 // Test to verify if function receives articles without an error
 test('Get general news', async() => {
-    const response = await request(app).get("/news/getNews/books")
+    const response = await request(app).get("/news/getNews?query=books")
     expect(response.statusCode).toBe(200)
     expect(response.body.articles.status).toBe("ok")
 })
+
+//Test to verify if gives error when wrong endpoint
+test('Get general news from wrong endpoing', async() => {
+    const response = await request(app).get("/news/getNews/WrongEndpoint")
+    expect(response.statusCode).toBe(404)
+
+})
+
+//Test to verify category
+test('Get right category for news articles', async() => {
+    const response = await request(app).get("/news/getNews?query=&category=entertainment")
+    expect(response.statusCode).toBe(200)
+
+
+})
+
+//Test to verify wrong category
+test('Get category wrong endpoint', async() => {
+    const response = await request(app).get("/news/getNews/WrongEndpoint")
+    expect(response.statusCode).toBe(404)
+
+})
+
+//Test to verify sources
+test('Get source of news', async() => {
+    const response = await request(app).get("/news/getNews?query=&sources=BBC-news")
+    expect(response.statusCode).toBe(200)
+    expect(response.body.articles.articles[0].source.name).toBe("BBC News")
+    console.log(response.body.articles)
+})
+
+//Test to verify bad endpoint
+test('Get source of news', async() => {
+    const response = await request(app).get("/news/getNews/WrongEndpoint")
+    expect(response.statusCode).toBe(404)
+    
+})
+
+
+
+
 
 
