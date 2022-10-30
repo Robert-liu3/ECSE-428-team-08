b9b9e76792aec625af0e7816e91f38a4cccb6f3d
Robert-liu3 <77682309+Robert-liu3@users.noreply.github.com>
2022-10-30 12:41:53 -0400

    Revert "User arturo dev"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,8 @@
-import NavBar from './components/Nav/NavBar.js'
-import Welcome from './components/Welcome/Welcome.js'
-import Charts from './components/Chart/Chart.js'
-import News from './components/News/News.js'
-import UserLogin from './components/UserLogin/UserLogin.js'
-import UserSignUp from './components/UserSignUp/UserSignUp.js'
-import './components/site_style.css'
-import { Route, Routes } from 'react-router-dom'
-import "bootstrap/dist/css/bootstrap.min.css"
+import NavBar from "./components/Nav/NavBar.js";
+import Welcome from "./components/Welcome/Welcome.js";
+import News from "./components/News/News.js";
+import "./components/site_style.css";
+import { Route, Routes } from "react-router-dom";
 
 function App(props) {
   return (
@@ -14,11 +10,8 @@ function App(props) {
       <NavBar />
       <div className="container">
         <Routes>
-          <Route path="/" element={<Welcome/>}/>
-          <Route path="/charts" element={<Charts/>}/>
-          <Route path="/news" element={<News/>}/>
-          <Route path="/login" element={<UserLogin/>}/>
-          <Route path="/signup" element={<UserSignUp/>}/>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/news" element={<News />} />
         </Routes>
       </div>
     </>
