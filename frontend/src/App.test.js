c4df502b0895c89bb4058b30447ae1196a7bf27a
happymario123 <52861139+happymario123@users.noreply.github.com>
2022-10-30 22:43:47 -0400

    Revert "add timeout legacy"
    This reverts commit 3280599473aa5031bbb47116704eb839b93e38dd.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -35,21 +35,18 @@ const run_chart_tests = async () => {
 }
 
 test('charts hello', () => {
-  jest.useFakeTimers('legacy')
   console.log("Hello");
 
   expect(true).toBe(true);
 });
 
 test('charts', async() => {
-  jest.useFakeTimers('legacy')
   let title = await run_chart_tests();
   console.log(title);
   expect(title).toBe('React App');
 });
 
 test("getTicker", async () => {
-  jest.useFakeTimers('legacy')
   let driver = await setupChromeDriver();
 
   await driver.wait(
