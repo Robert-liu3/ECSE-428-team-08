33b8b64e1792caeccbd0cd55962a8dcf630a6a7f
Jerry Xia <jerrylxia@gmail.com>
2022-11-26 13:51:41 -0500

    small changes

--- frontend/src/components/Chart/Watchlist/Watchlist.js
+++ frontend/src/components/Chart/Watchlist/Watchlist.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGetStockDetailsQuery } from "../../../services/stocksController";
 import Grid from "@mui/material/Grid"; // Grid version 1
 import "../styles.css";
@@ -9,66 +9,70 @@ export default function Watchlist() {
   const stock = "aapl";
   const { data, isFetching } = useGetStockDetailsQuery(stock);
 
-  // const [value, setValue] = useState("");
+  const [stocks, setStocks] = useState({
+    stocks: [data] * 6,
+  });
 
-  //   handleChange(e) {
-  //     setValue({ value: e.target.value });
-  //  };
+  const [inputField, setInputField] = useState({
+    stock: "",
+  });
 
-  //  keyPress(e) {
-  //     if(e.keyCode == 13){
-  //        console.log('value', e.target.value);
-  //     }
-  //  };
+  const inputsHandler = (e) => {
+    setInputField({ [e.target.name]: e.target.value });
+  };
+
+  const submitButton = () => {
+    alert(inputField.stock);
+  };
 
   return (
-    <div>
-      <Stack spacing={2}>
-        <Typography
-          variant="h4"
-          className="container_row"
-          style={{ border: 40 }}
+    // <div>
+    <Stack spacing={2} style={{ display: "flex", justifyContent: "center" }}>
+      <Typography variant="h4">My Watchlist</Typography>
+      {isFetching ? (
+        <></>
+      ) : (
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
         >
-          Watchlist
-        </Typography>
-        {isFetching ? (
-          <></>
-        ) : (
-          <Grid
-            container
-            // spacing={{ xs: 2, md: 3 }}
-            columns={{ xs: 4, sm: 8, md: 12 }}
-            className="container_row"
-          >
-            {Array.from(Array(6)).map((_, index) => (
-              <Grid xs={2} sm={4} md={4} key={index}>
-                <ListItem>
-                  <Card>
-                    <p style={{ padding: 20, paddingBottom: 0 }}>
-                      {data["Global Quote"]["01. symbol"]}
-                    </p>
-                    <p style={{ padding: 20, paddingTop: 0 }}>
-                      {data["Global Quote"]["02. open"]}
-                    </p>
-                  </Card>
-                </ListItem>
-              </Grid>
-            ))}
-            <Card style={{ padding: 20, paddingTop: 0 }}>
-              <div
-                style={{ padding: 20, paddingTop: 0, width: 100, height: 100 }}
-              >
-                <TextField
-                  id="standard-basic"
-                  // label="Add More"
-                  variant="standard"
-                />
-                <Button>Add</Button>
-              </div>
-            </Card>
-          </Grid>
-        )}
-      </Stack>
-    </div>
+          {Array.from(stocks.stocks).map((_, index) => (
+            <Grid item xs={2} sm={4} md={4} key={index}>
+              <Card style={{ paddingTop: 16 }}>
+                <p>{st["Global Quote"]["01. symbol"]}</p>
+                <p>{data["Global Quote"]["02. open"]}</p>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+      <Card
+        style={{
+          padding: 20,
+          paddingTop: 0,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            width: 100,
+            height: 100,
+          }}
+        >
+          <input
+            type="text"
+            name="stock"
+            onChange={inputsHandler}
+            placeholder="Stock Ticker"
+            value={inputField.stock}
+          />
+          <br />
+          <button onClick={submitButton}>Add</button>
+        </div>
+      </Card>
+    </Stack>
+    // </div>
   );
 }
