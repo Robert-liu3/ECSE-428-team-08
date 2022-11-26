ec6a9149ffef8578ea0beb45e79005052f2bae77
DannyTu2 <78064051+DannyTu2@users.noreply.github.com>
2022-11-25 21:53:08 -0500

    Added more tests for other functions, 10+ tests passed

--- backend/src/test/News.test.js
+++ backend/src/test/News.test.js
@@ -9,6 +9,14 @@ test('Get general news', async() => {
     expect(response.body.articles.status).toBe("ok")
 })
 
+//Test to verify with good endpoint
+// Test to verify if function gives error for non existent endpoint
+test('Get general news from rightendpoint', async() => {
+    const response = await request(app).get("/news/getNews/")
+    expect(response.statusCode).toBe(200)
+
+})
+
 
 // Test to verify if function gives error for non existent endpoint
 test('Get general news from wrong endpoint', async() => {
@@ -29,5 +37,48 @@ test('Get source of news', async() => {
     expect(response.statusCode).toBe(200)
     expect(response.body.articles.articles[0].source.name).toBe("BBC News")
 })
+
+//Test to verify another type of source 
+test('Get another type source of news', async() => {
+    const response = await request(app).get("/news/getNews?query=&sources=Independent")
+    expect(response.statusCode).toBe(200)
+    
+})
+
+//Test to verify wrong language of source
+test('Get wrong language of source', async() => {
+    const response = await request(app).get("/news/getNews=language=fr")
+    expect(response.statusCode).toBe(404)
+    
+})
+
+//Test to verify endpoint of adding favorite news
+test('Adding fav news', async() => {
+    const response = await request(app).post("/news/addFavNews")
+    expect(response.statusCode).toBe(200)
+    
+})
+
+//Test to verify endpoint of removing news
+test('Removing fav news', async() => {
+    const response = await request(app).delete("/news/removeFavNews")
+    expect(response.statusCode).toBe(200)
+    
+})
+
+//Test to verify endpoint of gettingAllArticles
+test('Getting all the articles in the database', async() => {
+    const response = await request(app).get("/news/getAllArticles")
+    expect(response.statusCode).toBe(200)
+    
+})
+//Test to verify endpoint of clearing all articles in the databse
+test('Clearing all the articles in the database', async() => {
+    const response = await request(app).delete("/news/clearArticles")
+    expect(response.statusCode).toBe(200)
+    
+})
+
+
 
 
