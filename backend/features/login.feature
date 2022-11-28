e8829fda2420291f0e33c6701ffeeafe30cd33c1
Robert-liu3 <robert.liu3@mail.mcgill.ca>
2022-11-27 23:51:10 -0500

    added

--- backend/features/login.feature
+++ backend/features/login.feature
@@ -1,7 +1,6 @@
 Feature: Login
 
 Scenario: tryna login
-
     Given user on page
-    When user enters username hi
-    Then user will get username hi
+    When user enters username "hi"
+    Then user will get username "hi"
--- backend/steps/login.steps.js
+++ backend/steps/login.steps.js
@@ -1,17 +1,17 @@
 import { defineFeature, loadFeature } from 'jest-cucumber';
 
-const feature = loadFeature('features/login.feature');
+const feature = loadFeature('../features/login.feature');
 
 defineFeature(feature, test => {
     test('login', ({ given, when, then }) => {
         given('Given user on page', () => {
             console.log("user is on page");
         });
-        when('When user enters username hi', () => {
+        when('When user enters username {string}', () => {
             console.log("when statement");
         });
-        then('Then user will get username hi', () => {
-
+        then('Then user will get username {string}', () => {
+            expect(true).toBe(true);
         });
     });
   });
