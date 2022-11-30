58f55f50b4d7f4ead1aecf66c78f005beb2d84ff
Jerry Xia <jerrylxia@gmail.com>
2022-11-29 20:05:53 -0500

    Test

--- backend/features/steps.js
+++ backend/features/steps.js
@@ -98,3 +98,77 @@ Then(
     assert.equal(found, false);
   }
 );
+
+When(
+  "user {string} enters password with {string}",
+  async function (userName, pass) {
+    // loginResponse = await axios.get('http://localhost:5000/login/Noah2/123');
+    try {
+      this.username = userName;
+      this.password = pass;
+      this.loginResponse = await axios.get(
+        `http://localhost:5000/login/${this.username}/${this.password}`
+      );
+    } catch (err) {
+      console.log("dasdasdasdasd" + err);
+    }
+  }
+);
+
+Then("user is {string}", async function (response) {
+  try {
+    assert.equal(this.loginResponse.data, response);
+  } catch (err) {
+    console.log(this.loginResponse + err);
+  }
+});
+
+//wrong username but right password
+When(
+  "wrong user {string} enters right password {string} for another user in the system",
+  async function (userName, pass) {
+    // loginResponse = await axios.get('http://localhost:5000/login/Noah2/123');
+    try {
+      this.username = userName;
+      this.password = pass;
+      this.loginResponse = await axios.get(
+        `http://localhost:5000/login/${this.username}/${this.password}`
+      );
+    } catch (err) {
+      console.log("dasdasdasdasd" + err);
+    }
+  }
+);
+
+Then("wrong user gets {string}", async function (response) {
+  try {
+    assert.equal(this.loginResponse.data, response);
+  } catch (err) {
+    console.log(this.loginResponse + err);
+  }
+});
+
+// wrong password but right username
+When(
+  "user {string} enters wrong password with {string}",
+  async function (userName, pass) {
+    // loginResponse = await axios.get('http://localhost:5000/login/Noah2/123');
+    try {
+      this.username = userName;
+      this.password = pass;
+      this.loginResponse = await axios.get(
+        `http://localhost:5000/login/${this.username}/${this.password}`
+      );
+    } catch (err) {
+      console.log("dasdasdasdasd" + err);
+    }
+  }
+);
+
+Then("user gets {string}", async function (response) {
+  try {
+    assert.equal(this.loginResponse.data, response);
+  } catch (err) {
+    console.log(this.loginResponse + err);
+  }
+});
